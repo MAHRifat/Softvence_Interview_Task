9f65c182b3e24fab6562cb45119eacb48e785990
MAHRifat <hasanrifata2@gmail.com>
2025-08-03 13:47:44 +0600

    Dashboard navber color change

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -105,7 +105,7 @@ const Dashboard = () => {
 
   if (loading) {
     return (
-      <div className="min-h-screen bg-gradient-to-br from-[#040612] to-[#60E5AE] flex flex-col justify-center items-center">
+      <div className="min-h-screen bg-gradient-to-br from-[#040612] to-[#135e3f] flex flex-col justify-center items-center">
         <svg className="animate-spin h-12 w-12 text-white mb-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
           <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
           <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
@@ -117,7 +117,7 @@ const Dashboard = () => {
 
   return (
     <div className="min-h-screen bg-gray-100 relative">
-      <div className="relative w-full h-[306px] bg-gradient-to-r from-[#60E5AE] to-[#040612] overflow-hidden">
+      <div className="relative w-full h-[306px] bg-gradient-to-r from-[#040612] to-[#135e3f] overflow-hidden">
         <img
           src="/login_img.png"
           alt="Top Illustration"
